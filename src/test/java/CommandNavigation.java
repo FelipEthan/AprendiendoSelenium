8c4fa8cb75476dee7d9a95c1bfba354d48960bc7
FelipEthan <afquimbayo7@gmail.com>
2024-07-15 22:46:43 -0500

    Se suben cambios

--- src/test/java/CommandNavigation.java
+++ src/test/java/CommandNavigation.java
@@ -5,31 +5,58 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommandNavigation {
     static WebDriver driver;
+    static WebDriverWait wait;
+    String url = "https://www.mercadolibre.com.co/categorias";
+    String elemento1 = "//i[@class='nav-icon-cart']";
+    String elemento2 = "//*[@id='user_id']";
+    String elemento3 = "//h1[contains(text(),'Categorías para comprar y vender')]";
+    String elemento4 = "//h1";
 
     @BeforeAll
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
     public void initDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
+
     @Test
-    public void testAmazon() {
-        driver.get("https://www.mercadolibre.com.co/categorias");
-        driver.findElement(By.xpath("//header/div[1]/div[6]/a[1]/i[1]")).click();
+    public void testMercadoLibre() {
+        driver.get(url);
+        WebElement element1 = driver.findElement(By.xpath(elemento1));
+        element1.click();
+        WebElement element2 = driver.findElement(By.xpath(elemento2));
+        element2.sendKeys("andrestyle.21@gmail.com");
         driver.navigate().back();
+        WebElement element3 = driver.findElement(By.xpath(elemento3));
+        String text = element3.getText();
+        System.out.println(text);
+        assertThat(element3.getText()).isEqualTo("Categorías para comprar y vender");
         driver.navigate().forward();
+        WebElement element4 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(elemento4)));
+        String text1 = element4.getText();
+        System.out.println(text1);
+        assertThat(element4.getText()).isEqualTo("Ingresa tu e‑mail, teléfono o usuario de Mercado Libre");
+        driver.navigate().to("https://www.mercadolibre.com.co/ofertas/supermercado#nav-header");
         driver.navigate().refresh();
-        driver.navigate().to("https://www.mercadolibre.com.co/gz/home/navigation#nav-header");
-        System.out.println("Prueba exitosa");
     }
+
     @AfterAll
     public static void cerrarDriver() {
         if (driver != null) {
--- src/test/java/DatePickerTest.java
+++ src/test/java/DatePickerTest.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import org.openqa.selenium.interactions.Actions;
 
-public class DatePickerTest{
+import java.time.LocalDate;
+
+
+public class DatePickerTest {
     private static WebDriver driver;
+    private static Actions actions;
 
     // Configuración inicial para descargar e instalar el driver de Chrome
     @BeforeAll
@@ -23,10 +25,51 @@ public class DatePickerTest{
     @BeforeEach
     public void initDriver() {
         driver = new ChromeDriver();
+        actions = new Actions(driver);
         driver.manage().window().maximize();
     }
 
     @Test
+    void testDatePicker() {
+        // Navegar a la página web de prueba
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        // Obtener la fecha de hoy
+        LocalDate today = LocalDate.now();
+        int currentYear = today.getYear();
+        int currentDay = today.getDayOfMonth();
+
+        String elemento1 = "//th[contains(text(),'July 2024')]";
+        String elemento2 = "//body[1]/div[1]/div[2]/table[1]/thead[1]/tr[2]/th[2]";
+        String elemento3 = "//body[1]/div[1]/div[3]/table[1]/thead[1]/tr[2]/th[1]";
+        String elemento4 = "//span[contains(text(),'1999')]";
+        String elemento5 = "//span[contains(text(),'Mar')]";
+        String elemento6 = "//td[contains(text(),'16')]";
+
+
+        // Encontrar y hacer clic en el campo de selección de fecha
+        WebElement datePicker = driver.findElement(By.name("my-date"));
+        datePicker.click();
+        WebElement element1 = driver.findElement(By.xpath(elemento1));
+        element1.click();
+        WebElement element2 = driver.findElement(By.xpath(elemento2));
+        element2.click();
+        WebElement element3 = driver.findElement(By.xpath(elemento3));
+        actions.doubleClick(element3).build().perform();
+        WebElement element4 = driver.findElement(By.xpath(elemento4));
+        element4.click();
+        WebElement element5 = driver.findElement(By.xpath(elemento5));
+        element5.click();
+        WebElement element6 = driver.findElement(By.xpath(elemento6));
+        element6.click();
+
+        // Obtener la fecha seleccionada del campo de selección de fecha
+        String oneYearBack = datePicker.getAttribute("value");
+        System.out.println("La fecha escogida es: " + oneYearBack);
+
+
+        /* Ejercicio para seleccionar la fecha actual
+            @Test
     void testDatePicker() {
         // Navegar a la página web de prueba
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
@@ -70,5 +113,7 @@ public class DatePickerTest{
 
         // Comprobar que la fecha seleccionada es la esperada
         // (aquí faltaría una aserción para comparar `oneYearBack` con `expectedDate`)
+    }
+         */
     }
 }
