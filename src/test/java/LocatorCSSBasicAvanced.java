cc512dc5a76953e226eff66c1c5025dbf9b73e64
FelipEthan <afquimbayo7@gmail.com>
2024-07-29 21:24:21 -0500

    Se suben cambios se trabaja con CSS selector

--- src/test/java/LocatorCSSBasicAvanced.java
+++ src/test/java/LocatorCSSBasicAvanced.java
@@ -5,9 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.opentest4j.AssertionFailedError;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+
+//Estudiar
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LocatorCSSBasicAvanced {
     private static WebDriver driver;
@@ -16,6 +22,15 @@ public class LocatorCSSBasicAvanced {
     String elemento1 = "[type=checkbox]:checked";
     String elemento2 = "[type=checkbox]:not(:checked)";
     String elemento3 = "[type=radio]:checked";
+    String elemento5 = "Signup";
+    String elemento6= "[name='name']";
+    String elemento7 = "[data-qa='signup-email']";
+    String elemento8 = "[data-qa='signup-button']";
+    String elemento9 = "[for='id_gender1']";
+    String elemento10 = "[id='password']";
+    String elemento11 = "[id='days']";
+    String elemento12 = "[id='months']";
+    String elemento13 = "[name='years']";
 
     @BeforeAll
     public static void setUp() {
@@ -69,6 +84,49 @@ public class LocatorCSSBasicAvanced {
         } finally {
             closeBrowser();
         }
+    }
+    @Test
+    public void testLocatorCSS2(){
+        driver.get("https://www.automationexercise.com/");
+        WebElement element = driver.findElement(By.partialLinkText(elemento5));
+        element.click();
+        WebElement element1 = driver.findElement(By.cssSelector(elemento6));
+        element1.sendKeys("Andres Quimbayo");
+        WebElement element2 = driver.findElement(By.cssSelector(elemento7));
+        element2.sendKeys("afquimbayo@gmail.com");
+        WebElement element3 = driver.findElement(By.cssSelector(elemento8));
+        element3.submit();
+        WebElement element4 = driver.findElement(By.cssSelector(elemento9));
+        element4.click();
+        WebElement element5 = driver.findElement(By.cssSelector(elemento10));
+        element5.sendKeys("Ethan");
+        WebElement element6 = driver.findElement(By.cssSelector(elemento11));
+        element6.sendKeys("21");
+        String actualdays = element6.getAttribute("value");
+        System.out.println("El día seleccionado es: " + actualdays);
+        WebElement dias = driver.findElement(By.cssSelector(elemento11));
+        Select select = new Select(dias);
+        List<WebElement> options = select.getOptions();
+        for (WebElement option : options){
+            System.out.println(option.getText());
+        }
+        WebElement meses = driver.findElement(By.cssSelector(elemento12));
+        meses.sendKeys("April");
+        String actualMonth = meses.getAttribute("value");
+        System.out.println("El mes seleccionado es " + actualMonth);
+        Select select1 = new Select(meses);
+        List<WebElement> options1 = select1.getOptions();
+        for (WebElement option2 : options1){
+            System.out.println(option2.getText());
+        }
+        WebElement year = driver.findElement(By.cssSelector(elemento13));
+        year.sendKeys("1999");
+        String actualyear = year.getAttribute("value");
+        System.out.println("Año seleccionada: " + actualyear);
+        System.out.println("La fecha seleccionada es: " + actualdays + "/" + actualMonth + "/"+ actualyear );
+
+
+
     }
 
     @AfterEach
